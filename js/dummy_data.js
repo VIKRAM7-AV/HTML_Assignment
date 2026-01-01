c819d30c4ed98719e93e98792f73704979621ae6
VIKRAM <vikramria195@gmail.com>
2026-01-02 01:20:54 +0530

    Most popular product developed

--- js/dummy_data.js
+++ js/dummy_data.js
@@ -30,3 +30,27 @@ export const perfume_Imgs = [
     img: "assets/img5.jpg",
   },
 ];
+
+export const fragranceDetails = [
+  {
+    title: "Original",
+    img: "assets/original.svg",
+    bestSeller: true,
+  },
+  {
+    title: "Lily",
+    img: "assets/Lily.svg",
+  },
+  {
+    title: "Rose",
+    img: "assets/rose.svg",
+  },
+];
+
+export const popularDescription = [
+  "1 bottle shipped monthly",
+  "Free Sampler for original, lily and rose fragrances",
+  "50% OFF Shipping ",
+  "Pause or Cancel Anytime after 3 months minimum",
+  "28 Day Money Back Guarantee*",
+];
